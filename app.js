26d4e1338f96a769b1386b6230f620a0e25ca560
AlexisMelo <Alexis.Robotiik@hotmail.fr>
2021-12-28 15:55:51 +0100

    refactor web application

--- app.js
+++ app.js
@@ -29,35 +29,27 @@ let players = {
 }
 let nextPlayer = null;
 let gameStarted = false;
-let gameFinished = false;
+let listMovesPlayed = []
 
 //Sockets
 
 io.on("connection", (socket) => {
 
-    console.log(`Connecté au client ${socket.id}`)
-
-    console.log(players)
+    console.log(`Client ${socket.id} connected`)
 
     socket.onAny((event, ...args) => {
-        console.log(`Server received : ${event} with ${args}`)
+        console.log(`Server received ${event} from ${socket.id} with args : ${args}`)
     })
 
-    //Connexion d'un joueur souhaitant participer au jeu (différent des controleurs qui font seulement regarder)
-
+    /**
+     * Connexion d'un joueur souhaitant participer au jeu (différent des controleurs qui font seulement regarder)
+     */
     socket.on("connect_player", (name) => {
-
-        //Vérifie si la partie a démarré pour autoriser la connexion des joueurs
-
-        if (!gameStarted) {
-            socket.broadcast.emit("connect_player_without_start", name)
-            return
-        }
 
         //Vérifie qu'un troisième joueur n'essaie pas de se connecter
 
         if (players.player1.name && players.player2.name && ![players.player2.name, players.player1.name].includes(name)) {
-            socket.broadcast.emit("alien_player", name)
+            socket.broadcast.emit("alien_client", name)
             return
         }
 
@@ -92,7 +84,7 @@ io.on("connection", (socket) => {
 
         if (!nextPlayer) {
             //https://stackoverflow.com/questions/9730966/how-to-decide-between-two-numbers-randomly-using-javascript
-            nextPlayer = Math.random() < 0.5 ? players.player1.name : players.player2.name
+            nextPlayer = getRandomFirstPlayer()
         }
 
         //On indique aux autres qu'un nouveau joueur vient de se connecter
@@ -102,79 +94,85 @@ io.on("connection", (socket) => {
         //Si 2 joueurs connectés, on "lance la partie"
 
         if (players.player1.name && players.player2.name) {
-            //Demande au joueur concerné de joueur un coup, io pas broadcast car si la socket concernée doit commencer elle doit recevoir l'event
-            io.emit("display_turn_player", nextPlayer)
+            //On répond direct à la socket avec le prochain joueur car si la socket c'est elle-même elle doit jouer direct
+            socket.emit("waiting_move", nextPlayer)
         }
     });
 
-    //Demande de début de partie
-
+    /**
+     * Demande de début de partie
+     * Si tous les joueurs sont connectés, on lance vraiment la partie
+     * Si un joueur manque ou n'est pas connecté, on indique que la partie ne peut pas démarrer à la socket concernée avec "start_game_error"
+     */
     socket.on("start_game", () => {
-        gameStarted = true;
-        io.emit("init_game", {board: p4.getBoard(), displayWaitingMsg: true})
-    });
-
-    //Reset du jeu
-
-    socket.on("reset_game", () => {
-        p4 = new Puissance4()
-        nextPlayer = Math.random() < 0.5 ? players.player1.name : players.player2.name
-        gameFinished = false
-        gameStarted = true
-        io.emit("init_game_after_reset", {board: p4.getBoard(), nextPlayer : nextPlayer});
-    });
-
-    //Reset après une déconnexion (un joueur sur deux déconnecté)
-
-    /*socket.on("reset_game_after_disconnected", (namePlayerConnected) => {
-        //reset du joueur déconnecté
-        if (namePlayerConnected === players.player1.name) players.player2 = {name: "", id: null};
-        else players.player1 = {name: "", id: null};
-        //reset de la partie
-        p4 = new Puissance4();
-        gameFinished = false;
-        io.emit("init_game", {board: p4.getBoard(), displayWaitingMsg: false});
-    });*/
-
-
-    //nouveau coup d'un joueur
-    socket.on("new_move", (move) => {
-        //dans cette fonction je ne vérifie pas que la colonne est comprise ente 0 et 6, ca doit etre verifier chez les players
-        //on vérifie que la partie n'est pas terminé
-        if (!gameFinished) {
-            //on vérifie que les deux joueurs sont bien connectés
-            if (players.player1.name && players.player2.name) {
-                //on vérifie que le joueur qui a fait le coup est bien le prochain joueur à jouer
-                if (move.name === nextPlayer) {
-                    let win = p4.newMove(move.column, move.name);
-                    //on envoie l'evenement du nouveau coup avec un plateau actualisé pour tous les clients
-                    io.emit("new_move", {board: p4.getBoard(), column: move.column, name: move.name});
-                    //dans le cas où toutes les cases sont remplies sans gagnant
-                    if (win === "nobody") {
-                        io.emit("nobody_win");
-                        gameFinished = true;
-                    }
-                    //dans le cas où on a un gagnant
-                    if (win === "players.player1" || win === "players.player2") {
-                        io.emit("win", move.name);
-                        gameFinished = true;
-                    } else {
-                        //changement de joueur actif
-                        if (players.player1.name === move.name) nextPlayer = players.player2.name;
-                        else nextPlayer = players.player1.name;
-                        io.emit("display_turn_player", nextPlayer);
-                    }
-                } else {
-                    io.emit("bad_player", move.name);
-                }
-            } else {
-                io.emit("missing_player");
-            }
+        if (players.player1.socket && players.player2.socket && players.player1.socket.connected && players.player2.socket.connected) {
+            resetEverything()
+            gameStarted = true
+            nextPlayer = getRandomFirstPlayer()
+            io.emit("start_game", {board: p4.getBoard(), nextPlayer: nextPlayer})
+        } else {
+            let playersConnected = getPlayersConnected()
+            socket.emit("start_game_error", playersConnected)
         }
     });
 
-    //Alerter les autres sockets qu'un joueur s'est déconnecté
+    /**
+     * Reset de la partie par un administrateur
+     */
+    socket.on("stop_game", () => {
+        resetEverything()
+        socket.broadcast.emit("stop_game");
+    });
 
+    /**
+     * Nouveau coup d'un joueur
+     */
+    socket.on("new_move", (move) => {
+        if (!gameStarted) {
+            //si la partie a pas démarré on fait rien
+            return
+        }
+
+        //on vérifie que le joueur qui a fait le coup est bien le prochain joueur à jouer
+        if (move.name !== nextPlayer) {
+            io.emit("bad_player", move.name);
+            return
+        }
+
+        let winner = p4.newMove(move.column, move.name);
+
+        //A CHANGER PAR SOCKET.BROADCAST.EMIT SI ON VEUT PAS QUE LE MEC RECOIVE SON PROPRE COUP
+        io.emit("new_move", {board: p4.getBoard(), column: move.column, name: move.name});
+
+        if (winner) {
+            //on a un gagnant
+            io.emit("end_game", winner === "nobody" ? winner : players[winner].name)
+            resetEverything()
+        } else {
+            //changement de joueur actif, winner = null
+            nextPlayer = players.player1.name === move.name ? players.player2.name : players.player1.name
+            io.emit("waiting_move", nextPlayer);
+            listMovesPlayed.push(move)
+        }
+    });
+
+    /**
+     * Une socket demande l'état actuel de la partie
+     */
+    socket.on("game_status", () => {
+        socket.emit("game_status", {
+            gameStarted: gameStarted,
+            board: p4.getBoard(),
+            listMovesPlayed: listMovesPlayed,
+            nextPlayer: nextPlayer,
+            playersConnected: getPlayersConnected()
+        })
+    })
+
+    /**
+     * Alerter toutes les socket autre que celle concernée qu'elle vient de se déconnecter
+     * Attention ! Utiliser disconnecting, pas disconnect !
+     */
     socket.on("disconnecting", () => {
         for (const [_, playerObject] of Object.entries(players)) {
             if (playerObject.socket && playerObject.socket.id === socket.id) {
@@ -185,6 +183,40 @@ io.on("connection", (socket) => {
     });
 
 });
+
+/**
+ * Obtenir le nom d'un des joueurs aléatoirement
+ * @returns {string}
+ */
+function getRandomFirstPlayer() {
+    return Math.random() < 0.5 ? players.player1.name : players.player2.name
+}
+
+/**
+ * Obtenir une liste avec tous les joueurs actuellement connectés
+ * @returns {*[]}
+ */
+function getPlayersConnected() {
+    let playersConnected = []
+
+    for (const [_, playerObject] of Object.entries(players)) {
+        if (playerObject.socket && playerObject.socket.connected) {
+            playersConnected.push(playerObject.name)
+        }
+    }
+
+    return playersConnected
+}
+
+/**
+ * Remettre tous les paramètres de la partie à 0
+ */
+function resetEverything() {
+    gameStarted = false
+    listMovesPlayed = []
+    nextPlayer = null
+    p4 = new Puissance4();
+}
 
 app.get("/", function (req, res) {
     res.sendFile(__dirname + "/index.html");
